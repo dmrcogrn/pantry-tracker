031a2e913f29cbae9cd15d91e7e43ee80fce3d8a
dmrcogrn <dmrcogrn@gmail.com>
2024-08-08 01:32:36 -0700

    Obfuscated secrets

--- firebase.js
+++ firebase.js
@@ -7,14 +7,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD0pM4zGxqMMoOJP8iQeWKx5h2STBEobFI",
-  authDomain: "hs-pantry-tracker-c78da.firebaseapp.com",
-  projectId: "hs-pantry-tracker-c78da",
-  storageBucket: "hs-pantry-tracker-c78da.appspot.com",
-  messagingSenderId: "639548224429",
-  appId: "1:639548224429:web:0d78bca2954f75a17a6724",
-  measurementId: "G-B2P8JN3ESV"
-};
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
